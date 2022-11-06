c0ba1a71e0b65d5c2631c18b35ef604549f7d16e
“manu_gum” <mj.montillavilar@gmail.com>
2022-11-06 21:42:27 +0100

    experimental: Disable Lint and TS errors to test deploy 💥

--- next.config.js
+++ next.config.js
@@ -4,7 +4,20 @@ const nextConfig = {
     appDir: true
   },
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  
 }
 
 module.exports = nextConfig
