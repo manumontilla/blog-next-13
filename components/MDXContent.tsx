f56ffca9cc357c60ae72671590035fd81049c5c8
“manu_gum” <mj.montillavilar@gmail.com>
2022-12-13 18:44:29 +0100

    feat: delete component codeblock. Changed by rehype

--- components/MDXContent.tsx
+++ components/MDXContent.tsx
@@ -1,16 +1,6 @@
 "use client";
 
 import { MDXRemote, MDXRemoteProps } from "next-mdx-remote";
-// import CardContent from "./MDXComponent";
-import CodeBlock from "./CodeBlock";
-
-// const components = {
-//   h1: (props:any) => <h1 style={{color: 'tomato'}} {...props} />,
-//   pre: (props:any) => <div {...props} />,
-//   code: CodeBlock
-// }
-
-const components = { CodeBlock };
 
 interface MDXContentProps {
   source: MDXRemoteProps;
