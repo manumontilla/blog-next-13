b7c46cd11e4a2860619b45fc84e6578800b21af8
“manu_gum” <mj.montillavilar@gmail.com>
2023-03-26 11:47:10 +0200

    fix tags

--- components/Tags.tsx
+++ components/Tags.tsx
@@ -4,7 +4,13 @@ const Tags = ({ tags }: string[]) => {
   return (
     <div className="card_tags">
       {tags.map((tag: string) => (
-        <span className={"tag tag-" + tag.toLowerCase()}>{tag}</span>
+        <span
+          className={
+            "tag tag-" + tag.toLowerCase().replace(".", "").replace("#", "")
+          }
+        >
+          {tag}
+        </span>
       ))}
     </div>
   );
